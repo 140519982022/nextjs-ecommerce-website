980e61753101319ef51f92d4792b27b21fe23143
nikita <you@excnikitakose1414@gmail.com>
2024-06-18 20:06:31 +0530

    header donne

--- src/app/components/common/Header.jsx
+++ src/app/components/common/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Col, NavLink, NavbarBrand, NavbarCollapse, NavbarToggle, Row } from 'react-bootstrap';
+"use client"
+
+import React, { useEffect } from 'react'
+import { Col, NavItem, NavLink, NavbarBrand, NavbarCollapse, NavbarToggle, Row } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../../assets/images/logo.png';
+// import logo from '../../assets/images/logo.png';
+import imag1 from '../../assets/images/logo.png'
 import '../../assets/css/header.css'
 import { IoIosSearch } from "react-icons/io";
 import { FaRegUserCircle } from "react-icons/fa";
@@ -13,23 +16,103 @@ import { FaRegHeart } from "react-icons/fa";
 import { MdOutlineLock } from "react-icons/md";
 
 export default function Header() {
+
+    useEffect(() => {
+        const bootstrap = import('bootstrap/dist/js/bootstrap.bundle.min.js')
+    }, []);
+
     return (
         <>
-            {/* <header className='container-fluid'> */}
             <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary shadow-lg position-sticky top-0 z-3">
                 <Container>
                     <NavbarBrand>
-                        <Image src={logo} />
-                        {/* <img src={logo} alt="logo" /> */}
+                        <Image src={imag1} />
                     </NavbarBrand>
 
                     <NavbarToggle aria-controls="responsive-navbar-nav" />
                     <NavbarCollapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <NavLink>Women</NavLink>
-                            <NavLink >Men</NavLink>
-                            <NavLink >Sale</NavLink>
-                            <NavLink >OurStory</NavLink>
+                            <NavItem>
+                                <NavLink>Women</NavLink>
+                                <div className='megaMenu'>
+                                    <Row className='p-2'>
+                                        <Col md={6}>
+                                            <Row className=''>
+                                                <Col md={4}>
+                                                    <NavLink className='fs-6'>Featured</NavLink>
+                                                    <NavLink className='our-link'>New In</NavLink>
+                                                    <NavLink className='our-link'>Best Sellers</NavLink>
+                                                    <NavLink className='our-link'>Linen, the fabric of summer</NavLink>
+                                                    <NavLink className='our-link'>The Originals</NavLink>
+                                                    <NavLink className='our-link'>Workwear</NavLink>
+                                                    <NavLink className='our-link'>Best of Instagram</NavLink>
+                                                    <NavLink className='our-link'>Gift Cards</NavLink>
+                                                    <NavLink className='our-link text-danger'>Sale</NavLink>
+                                                </Col>
+                                                <Col md={4}>
+                                                    <div>
+                                                        <NavLink className='fs-6'>Clothing</NavLink>
+                                                        <NavLink className='our-link'>Shop All</NavLink>
+                                                        <NavLink className='our-link'>T-shirts & Tops</NavLink>
+                                                        <NavLink className='our-link'>Blouses & Shirts
+                                                        </NavLink>
+                                                        <NavLink className='our-link'>Dresses & Jumpsuits</NavLink>
+                                                        <NavLink className='our-link'>Skirts & Shorts</NavLink>
+                                                        <NavLink className='our-link'>Swimwear
+                                                        </NavLink>
+                                                        <NavLink className='our-link'>Matching Sets</NavLink>
+                                                        <NavLink className='our-link'>Sweaters & Cardigans</NavLink>
+                                                        <NavLink className='our-link'>Blazers & Overshirts</NavLink>
+                                                        <NavLink className='our-link'>Jackets & Coats
+                                                        </NavLink>
+                                                        <NavLink className='our-link'>Denim
+                                                        </NavLink>
+                                                        <NavLink className='our-link'>Pants</NavLink>
+
+                                                    </div>
+
+                                                </Col>
+                                                <Col md={4}>
+                                                    <NavLink className='fs-6'>Accessories</NavLink>
+                                                    <NavLink className='our-link'>Shop All</NavLink>
+                                                    <NavLink className='our-link'>Caps & Hats</NavLink>
+                                                    <NavLink className='our-link'>Shoes & Boots
+                                                    </NavLink>
+                                                    <NavLink className='our-link'>Bags</NavLink>
+
+                                                </Col>
+                                            </Row>
+
+                                        </Col>
+                                        <Col md={6}>
+                                            <Row className='p-3'>
+                                                <Col md={6}>
+                                                    <Image src="https://www.frankandoak.com/cdn/shop/files/DRESSES_01_1_600x.jpg?v=1715888744" fluid />
+                                                </Col>
+
+                                                <Col md={6}>
+                                                    <Image src="https://www.frankandoak.com/cdn/shop/files/Tile_desktop_ea86bb3a-3987-4f5f-a049-f19784dbfa55_600x.jpg?v=1717698790" fluid/>
+
+
+                                                </Col>
+                                            </Row>
+
+                                        </Col>
+                                    </Row>
+                                </div>
+
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink>Men</NavLink>
+                                {/* <div className='megaMenu'></div> */}
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>Sale</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink>OurStory</NavLink>
+                            </NavItem>
                         </Nav>
                         <Nav>
                             <NavLink eventKey={2} href="#memes">
@@ -37,18 +120,11 @@ export default function Header() {
                             </NavLink>
                             <NavLink><FaRegUserCircle /></NavLink>
                             <NavLink><FaRegHeart /></NavLink>
-                            <NavLink><MdOutlineLock />
-                            </NavLink>
-
+                            <NavLink><MdOutlineLock /></NavLink>
                         </Nav>
                     </NavbarCollapse>
                 </Container>
             </Navbar>
-
-            
-
-            {/* </header> */}
-
         </>
     )
 }
